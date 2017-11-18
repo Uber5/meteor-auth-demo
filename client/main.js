ad67404805718e329b6b6443b0c540460f4e0931
Chris <chris@uber5.com>
2017-11-18 13:49:48 +0200

    Completed the example, including how to use token on client or server, but no error handling

--- client/main.js
+++ client/main.js
@@ -1,9 +1,32 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { sayHello } from 'meteor-u5auth'
+import { sayHello, getLiveToken } from 'meteor-u5auth'
 import { Meteor } from 'meteor/meteor'
 
 import './main.html';
+
+Template.login.helpers({
+  loginStatus() {
+    return Meteor.user() ? `logged in as ${Meteor.user().profile.email}` : 'not logged in'
+  }
+})
+
+Template.login.events({
+  'click button'(event, instance) {
+    Meteor.loginWithU5Auth({}, err => {
+      console.log('after attempting login, err', err)
+    })
+  }
+})
+
+Template.useToken.events({
+  'click button.client'(event, instance) {
+    getLiveToken().then(t => console.log('live token:', t))
+  },
+  'click button.server'(event, instance) {
+    Meteor.call('printToken') // should print token on server console
+  }
+})
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -24,3 +47,4 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
--- server/main.js
+++ server/main.js
@@ -1,7 +1,27 @@
 import { Meteor } from 'meteor/meteor';
-import { sayHello } from 'meteor-u5auth'
+import { sayHello, getLiveToken } from 'meteor-u5auth'
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
   sayHello()
+
+  ServiceConfiguration.configurations.upsert({
+    service: 'u5auth'
+  }, {
+    $set: {
+      clientId: process.env.OAUTH2_ID || 'your-client-id',
+      //loginStyle: 'redirect',
+      secret: process.env.OAUTH2_SECRET || 'your-client-secret',
+      issuer: process.env.OAUTH2_SITE || 'https://your-oauth2-provider.com',
+      requestPermissions: [ 'email', 'userinfo', 'openid', 'phone_number', 'sub' ]
+    }
+  })
+
 });
+
+Meteor.methods({
+  async printToken() {
+    const token = await getLiveToken()
+    console.log('token', token)
+  }
+})
